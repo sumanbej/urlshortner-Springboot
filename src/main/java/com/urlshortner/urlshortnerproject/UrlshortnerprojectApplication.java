e194f97dda4dd414f9d0c5c7ce4b1094367f72ef
sumanbej <sumanbej.glt@gmail.com>
2026-08-05 00:03:07 +0530

    first commit

---
+++ src/main/java/com/urlshortner/urlshortnerproject/UrlshortnerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.urlshortner.urlshortnerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortnerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortnerprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urlshortner/urlshortnerproject/UrlshortnerprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortner.urlshortnerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortnerprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
