90c4a1c4932111cd418f84e2207cf1c23b877ee4
sumanbej <sumanbej.glt@gmail.com>
2026-08-13 23:11:13 +0530

    Guava google lib Added

--- src/main/java/com/urlshortner/urlshortnerproject/Service/UrlServiceImpl.java
+++ src/main/java/com/urlshortner/urlshortnerproject/Service/UrlServiceImpl.java
@@ -2,11 +2,21 @@ package com.urlshortner.urlshortnerproject.Service;
 
 import com.urlshortner.urlshortnerproject.Model.Url;
 import com.urlshortner.urlshortnerproject.Model.UrlDto;
+import com.google.common.hash.Hashing;
+import org.springframework.util.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 public class UrlServiceImpl implements UrlService{
     @Override
     public Url generateShortLink(UrlDto urlDto) {
+        if(StringUtils.isEmpty(urlDto.getOriginalUrl())){
+            throw new IllegalArgumentException("URL cannot be empty");
+        }
+        String shortUrl=encodeUrl(urlDto.getOriginalUrl());
         return null;
+
     }
 
     @Override
@@ -22,5 +32,14 @@ public class UrlServiceImpl implements UrlService{
     @Override
     public void deleteShortLink(Url url) {
 
+    }
+    private String encodeUrl(String url)
+    {
+        String encodedUrl = "";
+        LocalDateTime time = LocalDateTime.now();
+        encodedUrl = Hashing.murmur3_32()
+                .hashString(url.concat(time.toString()), StandardCharsets.UTF_8)
+                .toString();
+        return  encodedUrl;
     }
 }
